f60f9426c313326942d834e6c91be807101992e3
MangataTS <1196991321@qq.com>
2023-08-25 00:32:40 +0800

    实现用户的战绩查询，提出某些预计实现功能

--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 
 ## 预计实现功能
 
-- [-] 用户的战绩查询（AC、WA、TLE、MLE、RE、CE、SE的次数）
-- [ ] 每日报备平台活跃量
-- [ ] 用户发布讨论贴时在群中同步发出
+- [x] 用户的战绩查询（AC、WA、TLE、MLE、RE、CE、SE的次数）
+- [x] 平台最近x小时提交数量（个人+全部）
 - [ ] 每日一题
+- [ ] 每日固定时间报备平台活跃量
+- [ ] 用户发布讨论贴时在群中同步发出
 - [ ] 作业推送(创建时推送到群里)
 - [ ] 提供Rank的Top10排名
 - [ ] 题目查询是否有题解
@@ -18,19 +19,28 @@
 
 
 ## 使用说明
-暂无，等多实现几个功能后再说，配置文件是 `MianConfig.json`，格式如下，目前正在思考
+暂无，等多实现几个功能后再说，配置文件是 `MianConfig.json`，需要自己创建一个，并放在根目录（`bot.py`同目录），格式如下，目前正在思考
 ```json
 {
-  "Hydro": {
-    "Host": "",
-    "Port": 27017,
-    "username": "hydro",
-    "password": "",
-    "authSource": "hydro"
-  }
+    "Hydro": {
+      "Host": "42.193.50.191",
+      "Port": 27017,
+      "username": "hydro",
+      "password": "ufaRqx7vkR2KgjUDkHeOuihX7NZVwR9z",
+      "authSource": "hydro",
+      "timeOff": 8
+    }
 }
 ```
+其中可能需要解释的是`timeOff`，这个是数据库偏移正常时间的量，可以百度一下，一般是8h，如果不偏移就设置为0即可
+
+命令：
+
+- `commit uid hour` 表示的是查询UID最近hour小时的提交数量，eg：`commit 3 24` 表示的就是查询uid为3的用户最近24h的提交数量
+- `user uid` 表示的是查询UID的提交战绩（AC、WA、TLE、MLE、RE、CE、SE的次数）
+
 
 ## 更新日志
 
+- 2023.8.24 实现xx最近xx小时的提交查询
 - 2023.8.23 实现用户的战绩查询，提出某些预计实现功能
--- src/plugins/Hydrooj/__init__.py
+++ src/plugins/Hydrooj/__init__.py
@@ -1,11 +1,13 @@
 import json
+import string
 
 import pymongo
 from nonebot import on_command
+from nonebot.internal.params import ArgPlainText
 from nonebot.rule import to_me
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
-from nonebot.adapters.onebot.v11 import Bot, Event
+from datetime import datetime, timedelta
 
 with open('MianConfig.json') as file:
     data = json.load(file)
@@ -18,7 +20,7 @@ myclient = pymongo.MongoClient(MianConfig['Hydro']['Host'], MianConfig['Hydro'][
                                authSource=MianConfig['Hydro']['authSource'])
 mydb = myclient["hydro"]
 
-def RecordQuery(uid:int):
+def record_query(uid:int):
     mycol = mydb["record"]
     usrcol = mydb["user"]
     CountNum={}
@@ -34,14 +36,78 @@ def RecordQuery(uid:int):
     ans = {"AC":CountNum[1],"WA":CountNum[2],"TLE":CountNum[3],"MLE":CountNum[4],"RE":CountNum[6],"CE":CountNum[7],"SE":CountNum[8],"name":name}
     return ans
 
-UserRecord = on_command("战绩查询", rule=to_me(), aliases={"战绩", "查战绩","find"}, priority=10, block=True)
-
+UserRecord = on_command("user", rule=to_me(), aliases={"战绩", "查战绩","战绩查询"}, priority=10, block=True)
 @UserRecord.handle()
-async def handle_function(args: Message = CommandArg()):
-    if args.extract_plain_text():
-        uid = int(args.extract_plain_text())
-        ans = RecordQuery(uid)
+async def handle_record_query(args: Message = CommandArg()):
+    if uid := args.extract_plain_text():
+        ans = record_query(int(uid))
         rs = str(ans["name"])+"\n战绩如下：\n"+"AC:\t" + str(ans["AC"])+"\nWA:\t"+str(ans["WA"])+"\nTLE:\t"+str(ans["TLE"])+"\nMLE:\t"+\
              str(ans["MLE"])+"\nRE:\t"+str(ans["RE"])+"\nCE:\t"+str(ans["CE"])+"\nSE:\t"+str(ans["SE"])
         await UserRecord.finish(str(rs))
 
+@UserRecord.got("uid",prompt="请输入查询用户的UID")
+async def got_record_query(uid:str = ArgPlainText()):
+    ans = record_query(int(uid))
+    rs = str(ans["name"]) + "\n战绩如下：\n" + "AC:\t" + str(ans["AC"]) + "\nWA:\t" + str(ans["WA"]) + "\nTLE:\t" + str(
+        ans["TLE"]) + "\nMLE:\t" + \
+         str(ans["MLE"]) + "\nRE:\t" + str(ans["RE"]) + "\nCE:\t" + str(ans["CE"]) + "\nSE:\t" + str(ans["SE"])
+    await UserRecord.finish(str(rs))
+
+
+
+# 服务器时区偏差需要 ＋ 8hour，
+# TODO 添加定时任务，每天凌晨发布最近24h、7days、1month、total的提交数量
+# TODO 用户A平台最近X小时的提交数量，当A为0时表示所有提交数量
+def record_commit_count(uid,findhour :int) :
+    collection = mydb['record']
+    now = datetime.now()
+    past_24_hours = now - timedelta(hours=findhour + int(MianConfig['Hydro']['timeOff']))
+    query = {
+        'judgeAt': {
+            '$gte': past_24_hours,
+            '$lt': now
+        }
+    }
+    if uid != 0:
+        query['uid'] = uid
+    count = collection.count_documents(query)
+    print('过去', findhour, '小时新增的数据量：', count)
+    return count
+
+# commit uid hour eg: commit 2 24
+RecordCommit = on_command("commit", rule=to_me(), aliases={"评测记录"}, priority=10, block=True)
+@RecordCommit.handle()
+async def handle_record_commit_count(args: Message = CommandArg()):
+    if AllRecord := args.extract_plain_text():
+        split_string = AllRecord.split(" ")  # 使用空格分割字符串
+        int_list = [int(x) for x in split_string]
+        uid = int_list[0]
+        findhour = int_list[1]
+        if findhour < 0 :
+            await RecordCommit.finish(f"好家伙，你家时间有负数吗？")
+        if findhour > 9000:
+            await RecordCommit.finish(f"时间太久远了，臣妾不想查了=_=")
+        ans = record_commit_count(uid,findhour)
+        if uid != 0:
+            Rstr = f"最近{findhour}小时，uid为{uid}的提交数量为：{ans}次~"
+        else:
+            Rstr = f"最近{findhour}小时，Onlinejudge的提交数量为：{ans}次~"
+        await RecordCommit.finish(Rstr)
+
+
+@RecordCommit.got("AllRecord",prompt="请问你想查询谁最近几小时的提交数量呢？\neg: 3 24 \n表示查询uid为3，最近24h的提交数量")
+async def got_record_commit_count(AllRecord:str = ArgPlainText()):
+    split_string = AllRecord.split(" ")  # 使用空格分割字符串
+    int_list = [int(x) for x in split_string]
+    uid = int_list[0]
+    findhour = int_list[1]
+    if findhour < 0:
+        await RecordCommit.finish(f"好家伙，你家时间有负数吗？")
+    if findhour > 9000:
+        await RecordCommit.finish(f"时间太久远了，臣妾不想查了=_=")
+    ans = record_commit_count(uid, findhour)
+    if uid != 0:
+        Rstr = f"最近{findhour}小时，uid为{uid}的提交数量为：{ans}次~"
+    else:
+        Rstr = f"最近{findhour}小时，Onlinejudge的提交数量为：{ans}次~"
+    await RecordCommit.finish(Rstr)
